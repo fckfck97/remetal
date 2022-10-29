8201efd12547a5a9921983963c5e073d65ff8d33
Your Name <you@example.com>
2022-10-28 23:25:21 -0500

    calculo total de los gastos

--- inventario/reporte.py
+++ inventario/reporte.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from django.shortcuts import redirect, render
 from .models import Producto, Gastos
 from django.utils import timezone, dateformat
 from django.contrib import messages
-
+from django.db.models import Sum
 
 def imprimir_inventario(request):
     try:
@@ -23,11 +24,21 @@ def imprimir_gastos(request):
     try:
         template_name="inventario/gastos/reporte_gastos.html"
         today = dateformat.format(timezone.now(), 'd-m-Y')
+        ano = dateformat.format(timezone.now(), 'Y')
+        mes = dateformat.format(timezone.now(), 'm')
         pro = Gastos.objects.filter(estado=True)
+
+        gastos = Gastos.objects.filter(fc__year=ano, fc__month=mes,estado=True).aggregate(Sum('monto_gastos'))
+        if gastos['monto_gastos__sum'] is None:
+            gastos['monto_gastos__sum'] = 0
+        print(gastos['monto_gastos__sum'])
         context={
             'request':request,
             'pro':pro,
             'hoy':today,
+            'total':gastos['monto_gastos__sum'],
+            'ano':ano,
+            'mes':mes,
         }
         return render(request,template_name,context)
     except:
