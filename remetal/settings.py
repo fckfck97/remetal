7c84b035638af3d74897e8e431f7d87aa17a519d
Your Name <you@example.com>
2022-11-16 12:54:20 +0000

    add

--- remetal/settings.py
+++ remetal/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-wvv*0v#w77+-2y(-v!q_r^r7-b()cmh08!fffi%0jej8@7$efc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
